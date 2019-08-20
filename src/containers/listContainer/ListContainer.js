5e1449798e2a77745839953bf56cc6df5f49bb3c
saravana-zt123 <saravanakumar.2701@gmail.com>
2019-08-20 23:52:20 +0530

    List container css issue fix and basic card rednering

--- src/containers/listContainer/ListContainer.js
+++ src/containers/listContainer/ListContainer.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import './ListContainer.css';
 
+import Card from '../../components/card/Card'
+
 export default class ListContainer extends React.Component {
   constructor(props) {
     super(props);
@@ -18,26 +20,44 @@ export default class ListContainer extends React.Component {
   }
   render() {
     return (
-      <div className="listContainer margin10">
-        <div className="overflow-x-auto flexgrow">
-          <div className="dflex flexcolumn">
-            <div className="flexshrink">
-              <div className="dflex flexrow alignVertical listHeader">
-                <div className="flexgrow">{this.props.title}</div>
-                <div className="flexshrink pointer" onClick={this.handleDelClick}>
-                  <svg style={this.svgStyle} viewBox="0 0 24 24">
-                    <path fill="#000000" d="M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19M8,9H16V19H8V9M15.5,4L14.5,3H9.5L8.5,4H5V6H19V4H15.5Z" />
-                  </svg>
-                </div>
-              </div>
-            </div>
-            <div className="flexgrow">
-
+      <div className="dflex flexcolumn h100 listContainer marginR20">
+        <div className="flexshrink">
+          <div className="dflex flexrow alignVertical listHeader">
+            <div className="flexgrow">{this.props.title}</div>
+            <div className="flexshrink pointer" onClick={this.handleDelClick}>
+              <svg style={this.svgStyle} viewBox="0 0 24 24">
+                <path fill="#000000" d="M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19M8,9H16V19H8V9M15.5,4L14.5,3H9.5L8.5,4H5V6H19V4H15.5Z" />
+              </svg>
             </div>
           </div>
         </div>
+        <div className="flexgrow overflow-y-auto">
+          <Card title="asd" />
+          <Card title="asd" />
+          <Card title="asd" />
+          <Card title="asd" />
+          <Card title="asd" />
+          <Card title="asd" />
+          <Card title="asd" />
+          <Card title="asd" />
+          <Card title="asd" />
+          <Card title="asd" />
+          <Card title="asd" />
+          <Card title="asd" />
+          <Card title="asd" />
+          <Card title="asd" />
+          <Card title="asd" />
+          <Card title="asd" />
+          <Card title="asd" />
+          <Card title="asd" />
+          <Card title="asd" />
+          <Card title="asd" />
+          <Card title="asd" />
+          <Card title="asd" />
+          <Card title="asd" />
+          <Card title="asd" />
+        </div>
       </div>
-
     )
   }
 }
